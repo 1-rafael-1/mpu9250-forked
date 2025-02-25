1213fdb75abc3629993bbea0404719f6164cfe52
1-rafael-1 <rafael.koch@gmx.net>
2025-02-25 20:35:01 +0100

    move existing examples into legacy examples

---
+++ examples/legacy/README.md
@@ -0,0 +1,5 @@
+# Examples - Old Style
+
+This folder contains the original examples from before the transition to embedded-hal 1.0. They have been preserved here as a reference but have not been updated to work with the current version of the library.
+
+These examples are provided for historical reference only. For up-to-date examples that work with the current version of the library, please refer to the main examples directory.
--- examples/legacy/bbblue_dmp.rs
+++ examples/legacy/bbblue_dmp.rs
@@ -11,9 +11,9 @@ extern crate mpu9250;
 
 use std::io::{self, Write};
 
-use hal::sysfs_gpio;
 use hal::Delay;
 use hal::I2cdev;
+use hal::sysfs_gpio;
 use mpu9250::DMP_FIRMWARE;
 use mpu9250::{Error, Mpu9250};
 
@@ -30,27 +30,25 @@ fn main() {
         let mut stdout = stdout.lock();
         writeln!(&mut stdout, "  Normalized quaternion").unwrap();
 
-        let mut mpu9250 =
-            Mpu9250::dmp_default(i2c, &mut Delay, &DMP_FIRMWARE).expect("unable to load firmware");
+        let mut mpu9250 = Mpu9250::dmp_default(i2c, &mut Delay, &DMP_FIRMWARE)
+            .expect("unable to load firmware");
 
         loop {
             match event.poll(1000).unwrap() {
-                Some(_) =>
-                    match mpu9250.dmp_all::<[f32; 3], [f64; 4]>() {
-                        Ok(measure) => {
-                            write!(&mut stdout,
-                                "\r{:?}",
-                                measure).unwrap();
-                            stdout.flush().unwrap();
-                        },
-                        Err(Error::DmpDataNotReady) => (),
-                        Err(_) => (),
+                Some(_) => match mpu9250.dmp_all::<[f32; 3], [f64; 4]>() {
+                    Ok(measure) => {
+                        write!(&mut stdout, "\r{:?}", measure).unwrap();
+                        stdout.flush().unwrap();
                     },
+                    Err(Error::DmpDataNotReady) => (),
+                    Err(_) => (),
+                },
                 None => {
                     write!(&mut stdout, "\nTimeout\n").unwrap();
                     mpu9250.reset_fifo(&mut Delay).unwrap();
-                }
+                },
             }
         }
-    }).unwrap();
+    })
+    .unwrap();
 }
